57d194111529d8f1fe5bcd91f26b0c99c5f2afc2
ELsrEUELR <1827998e@ummich.mx>
2025-12-16 10:20:10 -0600

    constructores

---
+++ src/main/java/com/example/biblioteca/model/Usuario.java
@@ -0,0 +1,19 @@
+package com.example.biblioteca.model;
+
+public class Usuario {
+    private String matricula;
+    private String rol;
+    private String nombre;
+    private String contrasena;
+    private String direccion;
+    private String telefono;
+    private String correo;
+
+    Usuario (){}
+
+    Usuario (String matricula,String rol,String nombre,String contrasena,String direccion,String telefono,String correo){
+        this.matricula = matricula;
+        this.rol = rol;
+        this.
+    }
+}
