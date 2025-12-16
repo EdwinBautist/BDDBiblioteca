bc571cf612e23f59baf2299f8c478532db63ffc3
edwi <2105732g@umich.mx>
2025-12-16 10:43:44 -0600

    Archivos DAO (Data Acces Object)

---
+++ src/main/java/com/example/biblioteca/config/config.java
@@ -0,0 +1,8 @@
+package com.example.biblioteca.config;
+import java.sql.*;
+//Establecemos la conexión con la base de datos.
+public class config {
+    public Connection conexion(){
+        Connection conn = null;
+    }
+}
