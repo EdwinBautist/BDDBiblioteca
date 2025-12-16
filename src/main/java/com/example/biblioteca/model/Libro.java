f8c9df57d84b0e4cf8d5b0732e50d270dd7658cb
ELsrEUELR <1827998e@ummich.mx>
2025-12-16 10:26:40 -0600

    constructores

--- src/main/java/com/example/biblioteca/model/Libro.java
+++ src/main/java/com/example/biblioteca/model/Libro.java
@@ -1,4 +1,13 @@
 package com.example.biblioteca.model;
 
 public class Libro {
+    private int id_libro;
+    private String titulo;
+    private int no_paginas;
+    private int anio_publicacion;
+    private int  ejemplares;
+    private String edicion;
+    private String descripcion;
+    private
+
 }
--- src/main/java/com/example/biblioteca/model/Usuario.java
+++ src/main/java/com/example/biblioteca/model/Usuario.java
@@ -9,11 +9,16 @@ public class Usuario {
     private String telefono;
     private String correo;
 
+    //constructores->
     Usuario (){}
 
     Usuario (String matricula,String rol,String nombre,String contrasena,String direccion,String telefono,String correo){
         this.matricula = matricula;
         this.rol = rol;
-        this.
+        this.nombre = nombre;
+        this.contrasena = contrasena;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.correo = correo;
     }
 }
